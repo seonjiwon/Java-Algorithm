7b9ee7c69ad072c6d175d6c7ad42d6963315f9f7
SeonJiWon <72730080+seonjiwon@users.noreply.github.com>
2026-03-03 01:16:29 +0900

    [Gold III] Title: 소수의 연속합, Time: 228 ms, Memory: 29332 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static final int MAX_NUMBER = 4_000_000;
+
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        List<Integer> primeList = new ArrayList<>();
+//
+//        for (int i = 2; i <= MAX_NUMBER; i++) {
+//            if (isPrime(i)) {
+//                primeList.add(i);
+//            }
+//        }
+//
+//        int target = Integer.parseInt(br.readLine());
+//
+//        int start = 0, end = 0, sum = 0, cnt = 0;
+//        while (start < primeList.size()) {
+//            if (sum >= target || end == primeList.size()) {
+//                if (sum == target) {
+//                    cnt++;
+//                }
+//                sum -= primeList.get(start++);
+//            } else {
+//                sum += primeList.get(end++);
+//            }
+//        }
+//
+//        System.out.println(cnt);
+//        br.close();
+//    }
+//
+//    private static boolean isPrime(int n) {
+//        for (int i = 2; i <= Math.sqrt(n); i++) {
+//            if (n % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        boolean[] isPrime = new boolean[MAX_NUMBER + 1];
+        Arrays.fill(isPrime, true);
+
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i <= MAX_NUMBER; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= MAX_NUMBER; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        List<Integer> primeList = new ArrayList<>();
+        for (int i = 0; i <= MAX_NUMBER; i++) {
+            if (isPrime[i]) {
+                primeList.add(i);
+            }
+        }
+
+        int target = Integer.parseInt(br.readLine());
+
+        int start = 0, end = 0, sum = 0, cnt = 0;
+        while (start < primeList.size()) {
+            if (sum >= target || end == primeList.size()) {
+                if (sum == target) {
+                    cnt++;
+                }
+                sum -= primeList.get(start++);
+            } else {
+                sum += primeList.get(end++);
+            }
+        }
+
+        System.out.println(cnt);
+
+        br.close();
+    }
+}
