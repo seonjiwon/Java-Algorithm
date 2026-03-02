91df7786ed8d654b9beb6e6ab1865f69f1e92e6b
SeonJiWon <72730080+seonjiwon@users.noreply.github.com>
2026-03-03 01:20:41 +0900

    [Silver IV] Title: 큐, Time: 268 ms, Memory: 20872 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<String> queue = new ArrayDeque<>();
+
+        while (N-- > 0) {
+            String[] split = br.readLine().split(" ");
+            switch (split[0]) {
+                case "push":
+                    queue.offer(split[1]);
+                    break;
+                case "pop":
+                    System.out.println(queue.isEmpty() ? -1 : queue.poll());
+                    break;
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    System.out.println(queue.isEmpty() ? 1 : 0);
+                    break;
+                case "front":
+                    System.out.println(queue.isEmpty() ? -1 : queue.peek());
+                    break;
+                case "back":
+                    System.out.println(queue.isEmpty() ? -1 : queue.peekLast());
+                    break;
+            }
+
+        }
+
+        br.close();
+    }
+}
