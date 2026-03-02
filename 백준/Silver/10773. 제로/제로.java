30c454551e726b1bf4a563cf0ac68ef37e35e5b8
SeonJiWon <72730080+seonjiwon@users.noreply.github.com>
2026-03-03 01:20:28 +0900

    [Silver IV] Title: 제로, Time: 192 ms, Memory: 25668 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        while (N-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        Integer result = stack.stream()
+                .reduce(0, Integer::sum);
+
+        System.out.println(result);
+
+        br.close();
+    }
+}
