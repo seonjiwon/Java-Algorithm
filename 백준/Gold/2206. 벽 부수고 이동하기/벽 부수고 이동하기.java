d86f341ee5065a0c98224fba6c7e5c84acf70e3f
SeonJiWon <72730080+seonjiwon@users.noreply.github.com>
2026-04-13 17:25:15 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 636 ms, Memory: 161288 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,69 +1,83 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static int N, M;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
     static int[][] map;
     static boolean[][][] visited;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] split = br.readLine().split(" ");
-        N = Integer.parseInt(split[0]);
-        M = Integer.parseInt(split[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
-        visited = new boolean[N][M][2];
-
         for (int i = 0; i < N; i++) {
-            String row = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = row.charAt(j) - '0';
+                map[i][j] = Integer.parseInt(String.valueOf(line.charAt(j)));
             }
         }
 
-        System.out.println(bfs());
+        visited = new boolean[2][N][M];
+        int result = bfs();
+
+        System.out.println(result);
         br.close();
-     }
+    }
 
-    private static int bfs(){
+    private static int bfs() {
         Queue<int[]> queue = new LinkedList<>();
-
-        queue.offer(new int[]{0, 0, 1, 0});
+        // startX, startY, crash, distance
+        queue.offer(new int[]{0, 0, 0, 1});
         visited[0][0][0] = true;
 
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
-            int x = current[1];
-            int y = current[0];
-            int distance = current[2];
-            int broken = current[3];
 
-            if (x == M - 1 && y == N - 1) {
+            int x = current[0];
+            int y = current[1];
+            int k = current[2];
+            int distance = current[3];
+
+            if (x == N - 1 && y == M - 1) {
                 return distance;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i], ny = y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-                    if (map[ny][nx] == 0 && !visited[ny][nx][broken]) {
-                        visited[ny][nx][broken] = true;
-                        queue.offer(new int[]{ny, nx, distance + 1, broken});
-                    } else if (map[ny][nx] == 1) {
-                        if (broken == 0 && !visited[ny][nx][1]) {
-                            visited[ny][nx][1] = true;
-                            queue.offer(new int[]{ny, nx, distance + 1, 1});
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+//                    System.out.println("nx: " + nx + ", ny: " + ny);
+                    if (k == 0) {
+                        if (map[nx][ny] == 0 && !visited[0][nx][ny]) {
+                            queue.offer(new int[]{nx, ny, k, distance + 1});
+                            visited[0][nx][ny] = true;
+                        } else if (map[nx][ny] == 1 && !visited[0][nx][ny]) {
+                            queue.offer(new int[]{nx, ny, k + 1, distance + 1});
+                            visited[0][nx][ny] = true;
+                        }
+                    } else {
+                        if (map[nx][ny] == 0 && !visited[1][nx][ny]) {
+                            queue.offer(new int[]{nx, ny, k, distance + 1});
+                            visited[1][nx][ny] = true;
                         }
                     }
                 }
             }
         }
+
         return -1;
     }
 }
