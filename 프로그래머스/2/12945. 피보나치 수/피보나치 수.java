c470a8a4c20de9753a0a2fa0e4783770b30a8031
SeonJiWon <72730080+seonjiwon@users.noreply.github.com>
2026-04-30 08:46:26 +0900

    [level 2] Title: 피보나치 수, Time: 2.63 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i = 2; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
+        
+        answer = dp[n] % 1234567;
+        
+        return answer;
+    }
+}
