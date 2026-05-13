1dc6531cb08b171edf5cf51482e15925067d5f4b
SeonJiWon <72730080+seonjiwon@users.noreply.github.com>
2026-05-13 09:02:01 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.10 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] dp = new long[n + 1];
+        
+        dp[1] = 1;
+        
+        if(n > 1) {
+            dp[2] = 2;    
+        }
+        
+        for(int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        
+        answer = dp[n];
+        
+        return answer;
+    }
+}
