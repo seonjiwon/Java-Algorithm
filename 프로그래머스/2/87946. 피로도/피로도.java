2cd5d64a9067069f4ccb7136e327314820b02fb6
SeonJiWon <72730080+seonjiwon@users.noreply.github.com>
2026-07-03 09:00:32 +0900

    [level 2] Title: 피로도, Time: 1.56 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    
+    int answer = 0;
+    
+    public int solution(int k, int[][] dungeons) {    
+        boolean[] visited = new boolean[dungeons.length];
+        
+        dfs(k, dungeons, visited, 0);
+        
+        return answer;
+    }
+    
+    private void dfs(int currentFatigue, int[][] dungeons, boolean[] visited, int count) {
+        answer = Math.max(answer, count);
+
+        for (int i = 0; i < dungeons.length; i++) {
+            if(!visited[i] && currentFatigue >= dungeons[i][0]) {
+                visited[i] = true;
+
+                dfs(currentFatigue - dungeons[i][1], dungeons, visited, count + 1);
+
+                visited[i] = false;
+            }
+        }
+    }
+}
